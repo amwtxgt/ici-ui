01d5fbe364af528cc0006c90cab4192a1f10ec92
amwtxgt <214529161@qq.com>
2018-04-26 15:38:49 +0800

    初始化

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 import iciButton from './components/ici-button.vue'
 import iciLoadin from './components/ici-loading.vue'
 
-export const components = {
-  iciButton,iciLoadin
+const components = {
+    iciButton, iciLoadin
 }
 
-const install = function(Vue) {
-  if (install.installed) return;
-  components.map(component => Vue.component(component.name, component));
+const install = function (Vue) {
+    if (install.installed) return;
+    Object.keys(components).forEach(val=> Vue.component(val,components[val]))
 };
 
 if (typeof window !== 'undefined' && window.Vue) {
-  install(window.Vue);
+    install(window.Vue);
 }
 
 export default {install};
