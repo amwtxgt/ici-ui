4a2253d29bb2b6153d2296fe80d2aa2dc9314545
amwtxgt <214529161@qq.com>
2018-06-22 09:38:15 +0800

    重构

--- src/components/ici-loading.vue
+++ src/components/ici-loading.vue
@@ -63,6 +63,7 @@
             position: absolute;
             width: 100%;
             height: 100%;
+          overflow: hidden;
             z-index: 10;
             top: 0;
             left: 0;
