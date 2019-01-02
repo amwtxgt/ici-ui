20c9312f5c780965423002baf8481d9b5ef90a45
amwtxgt <214529161@qq.com>
2019-01-02 15:44:14 +0800

    webpack 4

--- src/components/ici-scroll.vue
+++ src/components/ici-scroll.vue
@@ -18,7 +18,7 @@
       return {
         scrollHeight: 0, //滚动区域总高度
         scrollTop: 1, //滚动距离上面的位置
-        height: 0, //限度高度
+        height: 0, //窗体高度
         hasTop: false,//是否触顶，
         hasBottom: false,//是否触底
         loading: false, //正在加载中
@@ -28,6 +28,10 @@
       disabled: Boolean,
       onReachTop: Function, //触发顶部
       onReachBottom: Function, //触发底部
+      offset: {
+        type: Number,
+        default: 50,
+      }
     },
     directives: {
       observe: {
@@ -48,54 +52,58 @@
                 //监听结构发生变化
                 if (record.addedNodes && record.addedNodes.length) {
                   record.addedNodes.forEach(v => {
-                    if(v.clientHeight){
+                    if (v.clientHeight) {
                       top += v.clientHeight
                     }
                   })
                 }
                 //do any code
               }
-
             });
 
             if (_this.scrollTop == 0) {
-              top = top+100
+              top = top + 100
               el.scrollTop += top;
             }
-
           });
           observer.observe(el, config);
         }
       }
     },
-    created(){
-      if(this.onReachTop){
+    created() {
+      if (this.onReachTop) {
         this.hasTop = true;
         this.scrollTop = 0;
       }
     },
     methods: {
-      rollTop(){
-        this.$refs['scrollLoading'].scrollTop=0
+      rollTop() {
+        this.$refs['scrollLoading'].scrollTop = 0
         this.scrollTop = 0;
       },
-      rollBottom(){
-        this.$refs['scrollLoading'].scrollTop = 5000000
+      rollBottom() {
+        this.$refs['scrollLoading'].scrollTop = 5000000;
       },
       mousewheel(e) {
-        var t = this.$refs.scrollLoading;
-        this.scrollHeight = t.scrollHeight
-        this.height = t.parentElement.clientHeight;
-        this.scrollTop = t.scrollTop;
+
+        if (e.deltaY < 0) {
+          this.onScroll('top')
+        } else {
+          this.onScroll('bottom')
+        }
+
         if (this.loading || this.disabled || (!this.hasTop && !this.hasBottom)) return;
+
         if (e.deltaY < 0) {
 
           if (this.hasTop) {
             console.log('向上')
             this.startLoad('top');
           }
-        } else {
-          if (this.hasBottom ) {
+        }
+        else {
+
+          if (this.hasBottom) {
             this.startLoad('bottom');
             console.log('向下')
           }
@@ -104,7 +112,7 @@
       //顶部加载
       startLoad(type) {
         var reachFun = type == 'top' ? this.onReachTop : this.onReachBottom;
-        if(reachFun){
+        if (reachFun) {
           this.loading = true
           reachFun(() => {
             this.loading = false;
@@ -113,29 +121,44 @@
           });
         }
       },
-      onScroll(e) {
+      onScroll(direction) {
 
-        if(this.loading){
+        if (this.loading) {
           return false;
         }
-        var t = this.$refs.scrollLoading;
-        if(!t){
-          return ;
-        }
-        this.scrollHeight = t.scrollHeight
-        this.height = t.parentElement.clientHeight;
-        this.scrollTop = t.scrollTop;
 
-        if (this.scrollTop == 0) {
-          this.hasTop = true
-          this.hasBottom = false;
-        } else if (this.scrollHeight === this.height + this.scrollTop) {
-          this.hasTop = false
-          this.hasBottom = true
-        } else {
-          this.hasTop = false
-          this.hasBottom = false;
+        var t = this.$refs.scrollLoading;
+
+        if (!t) {
+          return;
         }
+
+        var tStyle = window.getComputedStyle(t)
+
+        this.scrollHeight = t.scrollHeight
+        this.height = parseInt(tStyle.height);
+        this.scrollTop = t.scrollTop;
+        if (direction === 'top') {
+
+          if (this.scrollTop <= this.offset) {
+            this.hasTop = true
+            this.hasBottom = false;
+            return
+          }
+        }
+        else {
+
+          if (this.scrollHeight <= this.height + this.scrollTop + this.offset) {
+            this.hasTop = false
+            this.hasBottom = true
+            return
+          }
+        }
+
+        this.hasTop = false
+        this.hasBottom = false;
+
+
       },
     },
   }
@@ -159,14 +182,14 @@
         height: 50px;
       }
     }
-    &::-webkit-scrollbar{
-      width:6px;
-      height:10px;
+    &::-webkit-scrollbar {
+      width: 6px;
+      height: 10px;
     }
-    &::-webkit-scrollbar-thumb, &::-webkit-scrollbar-thumb{
+    &::-webkit-scrollbar-thumb, &::-webkit-scrollbar-thumb {
       background-color: #888;
     }
-    &::-webkit-scrollbar-track, &::-webkit-scrollbar-track{
+    &::-webkit-scrollbar-track, &::-webkit-scrollbar-track {
       background-color: #f0f0f0;
     }
   }
