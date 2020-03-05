5b0ed39534d1e1a85432c863916cc88c0b6f022f
amwtxgt <214529161@qq.com>
2020-03-05 15:39:11 +0800

    1

---
+++ index.d.ts
@@ -0,0 +1,24 @@
+import Vue from 'vue'
+interface Interface {
+  iciButton:Vue.Component,
+  iciLoading:Vue.Component,
+  iciIcon:Vue.Component,
+  iciMultipleField:Vue.Component,
+  iciRadioField:Vue.Component,
+  iciInput:Vue.Component,
+  iciInputGroup:Vue.Component,
+  iciPopup:Vue.Component,
+  iciModal:Vue.Component,
+  iciSearch:Vue.Component,
+  iciHint:Vue.Component,
+  iciHeader:Vue.Component,
+  iciBgimg:Vue.Component,
+  iciDrawer:Vue.Component,
+  iciScroll:Vue.Component,
+  iciPoptip:Vue.Component,
+  iciColorIcon:Vue.Component,
+  install:Vue.PluginObject<any>,
+  funs:any,
+}
+
+export default Interface
