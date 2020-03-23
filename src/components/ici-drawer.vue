da7f963519bb5b68a7240a0ed3884d7f8bc3c10d
amwtxgt <214529161@qq.com>
2020-03-23 16:49:02 +0800

    1`

--- src/components/ici-drawer.vue
+++ src/components/ici-drawer.vue
@@ -2,7 +2,7 @@
   <transition name="drawer">
     <div class="ici-drawer" :style="{'z-index': zIndex}" v-show="value">
       <div class="ici-drawer-mask" @click.self="close" v-if="!hideMask"></div>
-      <div class="ici-drawer-body" :class="direction=='left'?'drawer-left':'drawer-right'" :style="bodyStyle">
+      <div class="ici-drawer-body" :class="directionClass" :style="bodyStyle">
         <slot></slot>
       </div>
     </div>
@@ -14,13 +14,19 @@
     name: "ici-drawer",
     data() {
       return {
-        translate3d: ''
+        translate3d: '',
+        des: {
+          left: 'drawer-left',
+          right: 'drawer-right',
+          top: 'drawer-top',
+          bottom: 'drawer-bottom',
+        }
       };
     },
     watch: {},
     props: {
       value: Boolean,
-      hideMask:Boolean,
+      hideMask: Boolean,
       width: {
         type: [Number, String],
         default: 450
@@ -37,10 +43,26 @@
     },
 
     computed: {
-      bodyStyle() {
-        let style = {
-          width: `${this.width}px`,
+      directionClass() {
+
+        if (this.des[this.direction]) {
+          return this.des[this.direction]
+        } else {
+          return this.des.right
         }
+      },
+      bodyStyle() {
+        let width = this.width + 'px'
+        if (typeof this.width === "string" && parseInt(this.width).toString() !== this.width) {
+          width = this.width;
+        }
+        let style = {}
+        if(this.direction==='top' || this.direction ==='bottom'){
+            style.height =  width;
+        }else{
+          style.width =  width;
+        }
+
         return style
       },
     },
--- src/examples/page-drawer.vue
+++ src/examples/page-drawer.vue
@@ -1,17 +1,37 @@
 <template>
   <div>
     <div class="title">ici-drawer 抽屉弹窗</div>
-    <baseComponent title="基本弹窗" html='<ici-drawer v-model="popup1"></ici-drawer>'>
+    <baseComponent title="基本弹窗" html='<ici-drawer v-model="popup1"></ici-drawer>
+<ici-button @click="popup1= true">打开弹窗</ici-button>'>
       <ici-drawer v-model="popup1"></ici-drawer>
       <ici-button @click="popup1= true">打开弹窗</ici-button>
     </baseComponent>
-    <baseComponent title="设置宽度 width=200" html='<ici-drawer v-model="popup2" width="200" hide-mask></ici-drawer>'>
-      <ici-drawer v-model="popup2" width="200" hide-mask></ici-drawer>
+    <baseComponent title="全屏弹窗" html='<ici-drawer v-model="popup2" width="100%"><ici-button @click="popup2=false">关闭</ici-button></ici-drawer>
+<ici-button @click="popup2= true">打开弹窗</ici-button>'>
+      <ici-drawer v-model="popup2" width="100%"><ici-button @click="popup2=false">关闭</ici-button></ici-drawer>
       <ici-button @click="popup2= true">打开弹窗</ici-button>
     </baseComponent>
-    <baseComponent title="设置方向 direction=left" html='<ici-drawer direction="left" v-model="popup3" width="500"></ici-drawer>'>
-      <ici-drawer direction="left" v-model="popup3" width="500"></ici-drawer>
+    <baseComponent title="设置宽度 width=200" html='<ici-drawer v-model="popup3" width="200" hide-mask><ici-button @click="popup3=false">关闭</ici-button></ici-drawer>
+<ici-button @click="popup3= true">打开弹窗</ici-button>'>
+      <ici-drawer v-model="popup3" width="200" hide-mask><ici-button @click="popup3=false">关闭</ici-button></ici-drawer>
       <ici-button @click="popup3= true">打开弹窗</ici-button>
+    </baseComponent>
+    <baseComponent title="设置方向 direction=left" html='<ici-drawer direction="left" v-model="popup4" width="500"></ici-drawer>
+<ici-button @click="popup4= true">打开弹窗</ici-button>'>
+      <ici-drawer direction="left" v-model="popup4" width="500"></ici-drawer>
+      <ici-button @click="popup4= true">打开弹窗</ici-button>
+    </baseComponent>
+
+    <baseComponent title="设置方向 direction=bottom" html='<ici-drawer direction="bottom" v-model="popup5" width="500"></ici-drawer>
+<ici-button @click="popup5= true">打开弹窗</ici-button>'>
+      <ici-drawer direction="bottom" v-model="popup5" width="500"></ici-drawer>
+      <ici-button @click="popup5= true">打开弹窗</ici-button>
+    </baseComponent>
+
+    <baseComponent title="设置方向 direction=top" html='<ici-drawer direction="top" v-model="popup6" width="500"></ici-drawer>
+<ici-button @click="popup6= true">打开弹窗</ici-button>'>
+      <ici-drawer direction="top" v-model="popup6" width="500"></ici-drawer>
+      <ici-button @click="popup6= true">打开弹窗</ici-button>
     </baseComponent>
   </div>
 </template>
